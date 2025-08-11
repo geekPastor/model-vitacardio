f34e93e432c4200b9fe8a9a728d7c87ce10c34f7
Geek Pastor <chrinovicmukeba123@gmail.com>
2025-08-11 22:39:52 +0200

    Try fix bug

--- api.py
+++ api.py
@@ -35,7 +35,3 @@ def analyser(patient_data: PatientData):
 @app.get("/test/")
 def health_check():
     return {"status": "healthy"}
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
-    app.run(host="0.0.0.0", port=port)
