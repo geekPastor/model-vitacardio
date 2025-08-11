dabaa8637e3004d325bc0cfd6636a9328bdcb63b
Geek Pastor <chrinovicmukeba123@gmail.com>
2025-08-11 22:19:42 +0200

    add main

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 import pandas as pd
+import os
 
 # Import des modèles et fonction depuis models.py
 from model import pipeline_logreg, rf_model, analyser_patient_avec_scores
@@ -34,3 +35,7 @@ def analyser(patient_data: PatientData):
 @app.get("/test/")
 def health_check():
     return {"status": "healthy"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
