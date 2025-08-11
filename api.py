d52db642d4daed8aa980314ae510ba243de9ce24
Geek Pastor <chrinovicmukeba123@gmail.com>
2025-08-11 17:58:08 +0200

    add test endpoint

--- api.py
+++ api.py
@@ -24,11 +24,6 @@ def analyser(patient_data: PatientData):
     result = analyser_patient_avec_scores(df, pipeline_logreg, rf_model)
     return result
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    
-
 @app.get("/test/")
 def health_check():
     return {"status": "healthy"}
